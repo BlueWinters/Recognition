ad15aadb82a0781d27eef0a0516223167402a220
HanxiongXiao <296241332@qq.com>
2018-04-18 14:30:43 +0800

    vggnet_se

--- vggnet/vggnet_se.py
+++ vggnet/vggnet_se.py
@@ -1,11 +1,6 @@
 
 import tensorflow as tf
 import tensorflow.contrib.slim as slim
-import time as time
-import tools.cifar10 as cifar10
-import tools.helper as helper
-
-
 
 
 class VggNet:
@@ -14,51 +9,67 @@ class VggNet:
         self.y = tf.placeholder(dtype=tf.float32, shape=[None, 10])
         self.ratio = 8
 
-    def se_conv2d(self, input, out_chl, kernel, stride, padding, activation, name, is_train):
+    def se_conv2d_batch_norm_relu(self, input, out_chl, is_train, name):
         with tf.variable_scope(name):
             ratio = self.ratio
             assert out_chl % ratio == 0
-            body = tf.layers.conv2d(input, out_chl, kernel, stride, padding=padding, activation=activation, name='conv2d')
+            body = tf.layers.conv2d(input, out_chl, (3, 3), (1, 1), padding='same', name='conv2d')
             body = tf.layers.batch_normalization(body, momentum=0.9, training=is_train, name='bn')
-            squeeze = tf.reduce_mean(body, axis=(1, 2), name='squeeze')
-            excitation = tf.layers.dense(squeeze, units=int(out_chl/ratio), activation=activation, name='dense1')
-            excitation = tf.layers.dense(excitation, units=int(out_chl), activation=tf.nn.sigmoid, name='dense2')
-            excitation = tf.reshape(excitation, shape=(-1, 1, 1, out_chl))
-            output = body * excitation
+            body = tf.nn.relu(body, name='relu')
+            with tf.name_scope('squeeze_excitation'):
+                squeeze = tf.reduce_mean(body, axis=(1, 2), name='squeeze')
+                excitation = tf.layers.dense(squeeze, units=int(out_chl/ratio), activation=tf.nn.relu, name='dense1')
+                excitation = tf.layers.dense(excitation, units=int(out_chl), activation=tf.nn.sigmoid, name='dense2')
+                excitation = tf.reshape(excitation, shape=(-1, 1, 1, out_chl))
+                output = body * excitation
             return output
-            # return body
+
+    def dropout(self, input, rate, is_train, name):
+        return tf.layers.dropout(input, rate=rate, training=is_train, name=name)
+
+    def max_pool(self, input, name):
+        return tf.layers.max_pooling2d(input, (2, 2), (2, 2), padding='valid', name=name)
 
     def forward(self, is_train, reuse, args=None):
         with tf.variable_scope('vggnet', reuse=reuse):
             body = self.x
-            body = self.se_conv2d(body, 64, (3, 3), (1, 1), 'same', tf.nn.relu, 'conv2d_1_1', is_train)
-            body = self.se_conv2d(body, 128, (3, 3), (1, 1), 'same', tf.nn.relu, 'conv2d_1_2', is_train)
-            body = tf.layers.max_pooling2d(body, (2, 2), (2, 2), padding='valid', name='maxpool_1')
+            body = self.se_conv2d_batch_norm_relu(body, 64, is_train, name='block1')
+            body = self.dropout(body, 0.3, is_train, name='dropout1')
+            body = self.se_conv2d_batch_norm_relu(body, 128, is_train, name='block2')
+            body = self.max_pool(body, name='maxpool1')
 
-            body = self.se_conv2d(body, 256, (3, 3), (1, 1), 'same', tf.nn.relu, 'conv2d_2_1', is_train)
-            body = self.se_conv2d(body, 256, (3, 3), (1, 1), 'same', tf.nn.relu, 'conv2d_2_2', is_train)
-            body = tf.layers.max_pooling2d(body, (2, 2), (2, 2), padding='valid', name='maxpool_2')
+            body = self.se_conv2d_batch_norm_relu(body, 128, is_train, name='block3')
+            body = self.dropout(body, 0.4, is_train, name='dropout2')
+            body = self.se_conv2d_batch_norm_relu(body, 128, is_train, name='block4')
+            body = self.max_pool(body, name='maxpool2')
 
-            body = self.se_conv2d(body, 512, (3, 3), (1, 1), 'same', tf.nn.relu, 'conv2d_3_1', is_train)
-            body = self.se_conv2d(body, 512, (3, 3), (1, 1), 'same', tf.nn.relu, 'conv2d_3_2', is_train)
-            body = tf.layers.max_pooling2d(body, (2, 2), (2, 2), padding='valid', name='maxpool_3')
+            body = self.se_conv2d_batch_norm_relu(body, 256, is_train, name='block5')
+            body = self.dropout(body, 0.4, is_train, name='dropout3')
+            body = self.se_conv2d_batch_norm_relu(body, 256, is_train, name='block6')
+            body = self.dropout(body, 0.4, is_train, name='dropout4')
+            body = self.se_conv2d_batch_norm_relu(body, 256, is_train, name='block7')
+            body = self.max_pool(body, name='maxpool3')
 
-            body = self.se_conv2d(body, 512, (3, 3), (1, 1), 'same', tf.nn.relu, 'conv2d_4_1', is_train)
-            body = self.se_conv2d(body, 512, (3, 3), (1, 1), 'same', tf.nn.relu, 'conv2d_4_2', is_train)
-            body = tf.layers.max_pooling2d(body, (2, 2), (2, 2), padding='valid', name='maxpool_4')
+            body = self.se_conv2d_batch_norm_relu(body, 512, is_train, name='block8')
+            body = self.dropout(body, 0.4, is_train, name='dropout5')
+            body = self.se_conv2d_batch_norm_relu(body, 512, is_train, name='block9')
+            body = self.dropout(body, 0.4, is_train, name='dropout6')
+            body = self.se_conv2d_batch_norm_relu(body, 512, is_train, name='block10')
+            body = self.max_pool(body, name='maxpool4')
 
             # flatten
             body = slim.flatten(body, scope='flatten')
             # output probability
-            logit = tf.layers.dense(body, 10, name='logits')
-            prediction = tf.nn.softmax(logit)
+            logits = tf.layers.dense(body, 10, name='logits')
+            prediction = tf.nn.softmax(logits)
 
             # loss
             l2_vars = tf.add_n([tf.nn.l2_loss(var) for var in tf.trainable_variables()])
             cross_entropy = tf.reduce_mean(
-                tf.nn.softmax_cross_entropy_with_logits(logits=logit, labels=self.y))
+                tf.nn.softmax_cross_entropy_with_logits(logits=logits, labels=self.y))
             weight_decay = 0.0001
             loss = weight_decay * l2_vars + cross_entropy
 
             return loss, prediction
+
 
