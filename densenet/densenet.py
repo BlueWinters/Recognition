5b101674a534e4bfb6af8291f5310377da23f347
HanxiongXiao <296241332@qq.com>
2018-04-24 11:49:12 +0800

    densenet参照源代码修改

--- densenet/densenet.py
+++ densenet/densenet.py
@@ -1,3 +1,5 @@
+#----------------------------------------------------
+# ref: https://github.com/liuzhuang13/DenseNet
 
 import tensorflow as tf
 import tensorflow.contrib.slim as slim
@@ -9,48 +11,65 @@ class DenseNet:
         self.x = tf.placeholder(tf.float32, (None, 32, 32, 3), 'x')
         self.y = tf.placeholder(tf.float32, (None, 10), 'y')
 
-    def block_layer(self, input, num_filter, is_train, name):
+    def block_layer(self, input, num_filter, bottle_neck, is_train, name):
         with tf.variable_scope(name):
-            body = tf.layers.batch_normalization(input, momentum=0.9, training=is_train, name='bn')
-            body = tf.nn.relu(body, name='relu')
-            body = tf.layers.conv2d(body, num_filter, (3, 3), (1, 1), 'same', use_bias=False, name='con2d')
-            return body
+            if bottle_neck == True:
+                # conv1
+                body = tf.layers.batch_normalization(input, momentum=0.9, training=is_train, name='bn1')
+                body = tf.nn.relu(body, name='relu1')
+                body = tf.layers.conv2d(body, 4*num_filter, (1, 1), (1, 1), 'same', use_bias=False, name='conv1')
+                body = tf.layers.dropout(body, rate=0.2, training=is_train, name='dropout1')
+                # conv2
+                body = tf.layers.batch_normalization(body, momentum=0.9, training=is_train, name='bn')
+                body = tf.nn.relu(body, name='relu2')
+                body = tf.layers.conv2d(body, num_filter, (3, 3), (1, 1), 'same', use_bias=False, name='conv2')
+                body = tf.layers.dropout(body, rate=0.2, training=is_train, name='dropout2')
+                return body
+            else:
+                body = tf.layers.batch_normalization(input, momentum=0.9, training=is_train, name='bn')
+                body = tf.nn.relu(body, name='relu')
+                body = tf.layers.conv2d(body, num_filter, (3, 3), (1, 1), 'same', use_bias=False, name='con2d')
+                body = tf.layers.dropout(body, rate=0.2, training=is_train, name='dropout')
+                return body
 
-    def denset_block(self, input, in_chl, num_layers, growth_chl, is_train, name):
+    def denset_block(self, input, in_chl, num_layers, growth_chl, bottle_neck, is_train, name):
         with tf.variable_scope(name):
             body = input
             for n in range(num_layers):
-                add_body = self.block_layer(body, growth_chl, is_train, 'layer{}'.format(n))
+                add_body = self.block_layer(body, growth_chl, bottle_neck, is_train, 'layer{}'.format(n))
                 body = tf.concat([body, add_body], 3, name='concat{}'.format(n))
                 in_chl += growth_chl
             return body, in_chl
 
-    def transition(self, input, out_chl, is_train, name):
+    def transition(self, input, out_chl, last, is_train, name):
         with tf.variable_scope(name):
-            body = tf.layers.batch_normalization(input, momentum=0.9, training=is_train, name='bn')
-            body = tf.nn.relu(body, name='relu')
-            body = tf.layers.conv2d(body, out_chl, (1, 1), (1, 1), 'same', use_bias=False, name='conv2d')
-            body = tf.layers.dropout(body, rate=0.2, training=is_train, name='dropout')
-            body = tf.layers.average_pooling2d(body, (2, 2), (2, 2), 'valid', name='avg_pool')
-            return body
+            if last == True:
+                body = tf.layers.batch_normalization(input, momentum=0.9, training=is_train, name='bn')
+                body = tf.nn.relu(body, name='relu')
+                # body = tf.layers.average_pooling2d(body, (2, 2), (2, 2), 'valid', name='avg_pool')
+                return body
+            else:
+                body = tf.layers.batch_normalization(input, momentum=0.9, training=is_train, name='bn')
+                body = tf.nn.relu(body, name='relu')
+                body = tf.layers.conv2d(body, out_chl, (1, 1), (1, 1), 'same', use_bias=False, name='conv2d')
+                body = tf.layers.dropout(body, rate=0.2, training=is_train, name='dropout')
+                body = tf.layers.average_pooling2d(body, (2, 2), (2, 2), 'valid', name='avg_pool')
+                return body
 
     def forward(self, is_train, reuse, args=None):
-        num_blocks = 5#args.num_blocks
-        num_layers = 7#args.num_layers
-        growth_chl = 12#args.growth_chl
+        num_blocks = 3  #args.num_blocks
+        num_layers = 8 #args.num_layers
+        growth_chl = 12 #args.growth_chl
+        bottle_neck = False
 
         with tf.variable_scope('densenet', reuse=reuse):
-            body = tf.layers.conv2d(self.x, 16, (3, 3), (1, 1), 'same', name='conv')
-            cur_chl = 16
+            cur_chl = growth_chl * 2
+            body = tf.layers.conv2d(self.x, cur_chl, (3, 3), (1, 1), 'same', name='conv')
 
             for n in range(num_blocks):
                 body, cur_chl = self.denset_block(body, cur_chl, num_layers, growth_chl,
-                                                  is_train, 'block{}'.format(n))
-                if n < num_blocks-1:
-                    body = self.transition(body, cur_chl, is_train, 'transition{}'.format(n))
-
-            body = tf.layers.batch_normalization(body, momentum=0.9, training=is_train, name='bn')
-            body = tf.nn.relu(body, name='relu')
+                                                  bottle_neck, is_train, 'dense_block{}'.format(n))
+                body = self.transition(body, cur_chl, True if n == num_blocks-1 else False, is_train, 'transition{}'.format(n))
 
             body = tf.reduce_mean(body, axis=(1, 2), name='gap')
             body = slim.flatten(body, scope='flatten')
